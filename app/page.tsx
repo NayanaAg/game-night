d07286c305b6dccc368b9fc989ffc0db2ebb1c45
Nayana Agrawal <101566994@georgebrown.ca>
2024-11-12 16:37:35 +0000

    initial commit + npm install

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Page() {
+    return <>
+        <div id="container">
+            <div className="row">
+                <div className="col-md-10 offset-md-1">
+                    <h1>welcome to my website.</h1>
+                </div>
+            </div>
+        </div>
+    </>
+  }
