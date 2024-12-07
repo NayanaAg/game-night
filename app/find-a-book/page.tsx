cfed39e94f7ff0ca2c9cfc4e2705c5e5d4f729ee
Nayana Agrawal <101566994@georgebrown.ca>
2024-12-07 17:11:54 +0000

    created array with unique genres out of Library

--- app/find-a-book/page.tsx
+++ app/find-a-book/page.tsx
@@ -1,14 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../global.css";
 import GlobalNav from "../components/nav";
 import Library from "../library.json";
 
 export default function Page() {
+
+    const genreList = Library.map(book => book.genre)
+    const allGenres = genreList.join().split(",")
+    const uniqueGenres = [...new Set(allGenres)]
+
+    const [filter, setFilter] = useState("");
+
+
     return <>
-    
+        {uniqueGenres.map(genre =>
+            <a href="#" className={"btn p-3 m-2 btn-primary " + (genre === filter ? "btn-focus" : "")} id="genre" onClick={(e) => { setFilter(genre) }
+            }>{genre}</a>
+        )}
+
+        <a href="#" onClick={(e) => {
+            setFilter("");
+        }}>Clear all</a>
+
+        
     </>
 }
 
